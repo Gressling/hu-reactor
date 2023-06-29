7512a339589871e4e63d2d334cc6024b308195c1
Gressling <thorsten.gressling@gressling.com>
2023-06-29 16:57:33 +0100

    Comments fixed

--- test/Blinker/Blinker.ino
+++ test/Blinker/Blinker.ino
@@ -1,11 +1,11 @@
-#include Blinker.h
+#include "Blinker.h"
 
-Blinker ledBlinker(2);  LED is on pin 2 for Lolin board
+Blinker ledBlinker(2);  // LED is on pin 2 for Lolin board
 
 void setup() {
-   no setup needed
+   // no setup needed
 }
 
 void loop() {
-  ledBlinker.blink(1000);  Blink every 1000 ms or 1 second
+  ledBlinker.blink(1000);  // Blink every 1000 ms or 1 second
 }
