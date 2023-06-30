16d4423df97cabc2a32d5f3014e2621de8cfc455
Gressling <thorsten.gressling@gressling.com>
2023-06-30 15:24:15 +0100

    Create properties.h

---
+++ libraries/Internet/Internet/properties.h
@@ -0,0 +1,15 @@
+#ifndef PROPERTIES_H
+#define PROPERTIES_H
+
+#define WIFI_SSID "phase2"
+#define WIFI_PASSWORD "mamete123."
+
+#define HOST "http://labinformatics.gearhostpreview.com/v1/";
+
+#define TEST "test.php";
+#define TEST1 "sendval"
+#define TEST2 "sendval2"
+
+#endif
+
+
